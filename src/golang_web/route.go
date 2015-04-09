4c4fd0e63d5c00c74f1a05410f437e8218469193
LCore <kiritor@hotmail.com>
2015-04-09 14:45:30 +0800

    设置路由

---
+++ src/golang_web/route.go
@@ -0,0 +1,96 @@
+/**
+ 路由设置
+*/
+package main
+
+import (
+    "net/http"
+    "strings"
+    "reflect"
+    "log"
+    "html/template"
+)
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+    // 获取cookie
+    cookie, err := r.Cookie("admin_name")
+    if err != nil || cookie.Value == ""{
+		log.Println("重定向到登录界面")
+        http.Redirect(w, r, "/login", http.StatusFound)
+    }
+
+    pathInfo := strings.Trim(r.URL.Path, "/")
+    parts := strings.Split(pathInfo, "/")
+    var action = ""
+    if len(parts) > 1 {
+        action = strings.Title(parts[1]) + "View"
+    }else {
+		action =strings.Title(parts[0]+"View")
+	}
+
+    admin := &urlController{}
+    controller := reflect.ValueOf(admin)
+    method := controller.MethodByName(action)
+    if !method.IsValid() {
+        method = controller.MethodByName(strings.Title("index") + "View")
+    }
+    requestValue := reflect.ValueOf(r)
+    responseValue := reflect.ValueOf(w)
+    userValue := reflect.ValueOf(cookie.Value)
+    method.Call([]reflect.Value{responseValue, requestValue, userValue})
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/" {
+        http.Redirect(w, r, "/login", http.StatusFound)
+    }
+
+    t, err := template.ParseFiles("template/html/404.html")
+    if (err != nil) {
+        log.Println(err)
+    }
+    t.Execute(w, nil)
+}
+
+func urlHandler(w http.ResponseWriter,r *http.Request) {
+	pathInfo := strings.Trim(r.URL.Path, "/")
+    parts := strings.Split(pathInfo, "/")
+	log.Println(pathInfo)
+    var action = ""
+    if len(parts) > 1 {
+        action = strings.Title(parts[1]) + "View"
+    }else {
+		action = strings.Title(parts[0]) + "View"
+	}
+
+    login := &urlController{}
+    controller := reflect.ValueOf(login)
+    method := controller.MethodByName(action)
+    if !method.IsValid() {
+        method = controller.MethodByName(strings.Title("index") + "View")
+    }
+	log.Println(action)
+    requestValue := reflect.ValueOf(r)
+    responseValue := reflect.ValueOf(w)
+    method.Call([]reflect.Value{responseValue, requestValue})
+}
+
+func ajaxHandler(w http.ResponseWriter,r *http.Request) {
+	pathInfo := strings.Trim(r.URL.Path, "/")
+    parts := strings.Split(pathInfo, "/")
+    var action = ""
+    if len(parts) > 1 {
+        action = strings.Title(parts[1]) + "Action"
+    }else {
+		action = strings.Title(parts[0]) + "Action"
+	}
+    ajax := &ajaxController{}
+    controller := reflect.ValueOf(ajax)
+    method := controller.MethodByName(action)
+    if !method.IsValid() {
+        method = controller.MethodByName(strings.Title("index") + "Action")
+    }
+    requestValue := reflect.ValueOf(r)
+    responseValue := reflect.ValueOf(w)
+    method.Call([]reflect.Value{responseValue, requestValue})
+}
